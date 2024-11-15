e7c1d04cc43fb69e593df003328e81441f07a23a
Heondu <1217pgy@naver.com>
2024-11-16 01:38:50 +0900

    FIX : 왼손 오른손 콜라이더 안 겹치게 수정 Fix : 에임 수정

--- Assets/Scripts/MagicInputController.cs
+++ Assets/Scripts/MagicInputController.cs
@@ -4,6 +4,7 @@ public class MagicInputController : MonoBehaviour
 {
     public Projectile projectile;
     public Transform firePoint;
+    public bool isLeftHand;
 
     private Camera cam;
     private Vector3 destination;
@@ -24,8 +25,10 @@ public class MagicInputController : MonoBehaviour
         //ray.origin = transform.position;
         //ray.direction = transform.forward;
         RaycastHit hit;
+        int layerMask = 1 << LayerMask.NameToLayer("Object");
 
-        destination = Physics.Raycast(ray, out hit) ? hit.point : ray.GetPoint(1000);
+        destination = Physics.Raycast(ray, out hit, layerMask) ? hit.point : ray.GetPoint(1000);
+        //destination = ray.GetPoint(10);
         MoveProjectile();
     }
 
