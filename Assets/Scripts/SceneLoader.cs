75cf4e796e64733d7ea1065efaf2a4ed1d682a12
yucheon6000 <yucheon6000@gmail.com>
2024-11-22 00:43:59 +0900

    Add a hand menu and Fix scene bug

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,8 +3,42 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private static bool hasMainSceneLoaded = false;
+
+    private void Awake()
+    {
+        LoadMainSceneOnApplicationStarted();
+    }
+
+    private void LoadMainSceneOnApplicationStarted()
+    {
+        if (hasMainSceneLoaded == false)
+        {
+            SceneManager.sceneLoaded += (scene, loadMode) =>
+            {
+                if (loadMode == LoadSceneMode.Additive)
+                    SceneManager.SetActiveScene(scene);
+            };
+
+            SceneManager.LoadScene("MainScene", LoadSceneMode.Additive);
+
+            hasMainSceneLoaded = true;
+        }
+    }
+
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    public void ReloadScene()
+    {
+        var currentScene = SceneManager.GetActiveScene();
+        var index = currentScene.buildIndex;
+
+        SceneManager.UnloadSceneAsync(currentScene);
+
+        SceneManager.LoadScene(index, LoadSceneMode.Additive);
     }
 }
